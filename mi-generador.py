e99c155389444066d75f7c6b8a14f849fc0aa506
franzimbi <fzimbimbakis@fi.uba.ar>
2025-08-22 19:54:58 -0300

    sin querer borre el script

--- mi-generador.py
+++ mi-generador.py
@@ -3,19 +3,44 @@ import sys
 archivo_salida = sys.argv[1]
 cantidad_clientes = int(sys.argv[2])
 
+def crear_server():
+    return """  server:
+    container_name: server
+    image: server:latest
+    entrypoint: python3 /main.py
+    environment:
+      - PYTHONUNBUFFERED=1
+      - LOGGING_LEVEL=DEBUG
+    networks:
+      - testing_net
+"""
+
+def crear_cliente(id):
+    return f"""  client{id}:
+    container_name: client{id}
+    image: client:latest
+    entrypoint: /client
+    environment:
+      - CLI_ID={id}
+      - CLI_LOG_LEVEL=DEBUG
+    networks:
+      - testing_net
+    depends_on:
+      - server
+"""
+
+def crear_red():
+    return """networks:
+  testing_net:
+    ipam:
+      driver: default
+      config:
+        - subnet: 172.25.125.0/24
+"""
+
 with open(archivo_salida, 'w') as f:
     f.write("name: tp0\nservices:\n")
-    f.write("  server:\n    container_name: server\n    image: server:latest" \
-    "\n    entrypoint: python3 /main.py" \
-    "\n    environment:\n      - PYTHONUNBUFFERED=1\n      - LOGGING_LEVEL=DEBUG" \
-    "\n    networks:\n      - testing_net\n\n")
-
+    f.write(crear_server())
     for i in range(1, cantidad_clientes +1):
-        f.write(f"  client{i}:")
-        f.write(f"\n    container_name: client{i}")
-        f.write("\n    image: client:latest\n    entrypoint: /client\n    environment:\n")
-        f.write(f"      - CLI_ID={i}")
-        f.write("\n      - CLI_LOG_LEVEL=DEBUG\n    networks:\n      - testing_net\n    depends_on:\n      - server\n\n")
-
-    f.write("networks:\n  testing_net:\n    ipam:\n      driver: default\n      config:\n        - subnet: 172.25.125.0/24\n")
-
+        f.write(crear_cliente(i) + "\n")
+    f.write(crear_red())
