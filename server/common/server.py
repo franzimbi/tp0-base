616dbd70ca926da4452c30399301dc7e5a2a18c6
franzimbi <fzimbimbakis@fi.uba.ar>
2025-08-24 17:28:41 -0300

    cambio exit de server

--- server/common/server.py
+++ server/common/server.py
@@ -13,6 +13,7 @@ class Server:
         if self._server_socket:
             self._server_socket.close()
             logging.info("action: close_socket | result: success")
+        exit(0)
 
     def run(self):
         """
